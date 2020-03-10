8c771ff5195cf9f01e3ed80b08765e95b50a66f4
ysl3000 <yannicklamprecht@live.de>
2020-03-10 23:16:35 +0100

    task: Added a little convenience, fixed test and added sample with setup teardown

---
+++ src/main/java/kz/qwertukg/remoteWebDriver.kt
@@ -0,0 +1,25 @@
+package kz.qwertukg
+
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
+import org.openqa.selenium.remote.RemoteWebDriver
+
+fun chromeDriverWithOptions(init: ChromeOptions.() -> Unit): RemoteWebDriver {
+    val options = ChromeOptions()
+    init(options)
+    return ChromeDriver(options)
+}
+
+fun firefoxDriverWithOptions(init: FirefoxOptions.() -> Unit): RemoteWebDriver {
+    val options = FirefoxOptions()
+    init(options)
+    return FirefoxDriver(options)
+}
+
+
+fun RemoteWebDriver.use(init: RemoteWebDriver.() -> Unit): RemoteWebDriver{
+    init()
+    return this
+}
--- src/test/java/kz/qwertukg/WebDriverTestWithoutSetupTeardownMethods.kt
+++ src/test/java/kz/qwertukg/WebDriverTestWithoutSetupTeardownMethods.kt
@@ -1,29 +1,19 @@
 package kz.qwertukg
 
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
-import org.openqa.selenium.remote.RemoteWebDriver
 import kotlin.test.assertTrue
 
-class WebDriverKtTest {
-
-    private lateinit var driver: RemoteWebDriver
-
-    @Before
-    fun setup(){
-        driver = chromeDriverWithOptions {
-            setHeadless(true)
-            // setBinary("c:/chromedriver.exe") // no need for mac os when installed with brew etc.
-        }
-    }
+class WebDriverTestWithoutSetupTeardownMethods {
 
 
     @Test
     fun `test chromeDriver, elementById, wait, elementVisibilityById, elementVisibilityBySelector functions on google`() {
         var result = ""
 
-        driver.use {
+        chromeDriverWithOptions {
+            setHeadless(true)
+            // setBinary("c:/chromedriver.exe") // no need for mac os when installed with brew etc.
+        }.use {
             get("http://google.com")
 
             elementByName("q") {
@@ -39,13 +29,8 @@ class WebDriverKtTest {
                     }
                 }
             }
-        }
+        }.quit()
 
         assertTrue { result.contains("Kotlin Programming Language") }
-    }
-
-    @After
-    fun teardown(){
-        driver.quit()
     }
 }
